c4b68c279fa22378d2ac99de32fe642beeb6a6e9
Ana-Kaleb <Ana-Kaleb@test.com>
2022-05-23 10:36:30 -0300

    Exercicio Pneu

---
+++ src/Bomba.java
@@ -0,0 +1,30 @@
+
+public class Bomba {
+	private int presFinal;
+	private int presInic;
+	
+	public int resultado() 
+	{
+		return this.presFinal - this.presInic;
+	}
+
+	public int getPresFinal() {
+		return presFinal;
+	}
+
+	public void setPresFinal(int presFinal) {
+		this.presFinal = presFinal;
+	}
+
+	public int getPresInic() {
+		return presInic;
+	}
+
+	public void setPresInic(int presInic) {
+		this.presInic = presInic;
+	}
+
+	
+	
+	
+}
---
+++ src/TestaBomba.java
@@ -0,0 +1,14 @@
+
+public class TestaBomba {
+public static void main(String[] args) {
+	Bomba b1 = new Bomba();
+	
+	b1.setPresInic(30);
+	b1.setPresFinal(27);
+	
+	
+	System.out.println("Resultado: " + b1.resultado());
+	
+	
+}
+}
